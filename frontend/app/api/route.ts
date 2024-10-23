b0f2f58df49ba42fd5fd138087af5ffa63306ece
rohan <rohanjaiswal511@gmail.com>
2024-10-24 03:19:02 +0530

    Improved error handling for api calls

--- frontend/app/api/route.ts
+++ frontend/app/api/route.ts
@@ -8,112 +8,72 @@ const config = {
     }
 };
 
-export const createRule = async ( name: string, rule: string) => {
-    return await axios.post(`${API_URL}/create_rule`, { rule, name }, config)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error creating rules:', error);
-                throw error;
-            }
-        });
+const handleApiError = (error: unknown): never => {
+  if (axios.isAxiosError(error)) {
+    // console.error(error.response?.data.detail);
+    throw error.response?.data?.detail;
+  }
+  throw new Error('An unexpected error occurred');
 };
 
-export const combineRules = async (rules: string[], operator: string) => {
-    return axios.post(`${API_URL}/combine_rules`, { rules, operator}, config)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error combining rules:', error);
-                throw error;
-            }
-        });
+export const createRule = async ( name: string, rule: string) => {
+    try{
+        return await axios.post(`${API_URL}/create_rule`, { rule, name }, config).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
+};
+
+export const combineRules = async (rules: string[], operator: 'AND' | 'OR') => {
+    try{
+        return axios.post(`${API_URL}/combine_rules`, { rules, operator}, config).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 };
 export const evaluateRule = async (ruleName: string, data: Record<string, string>) => {
-    return axios.post(`${API_URL}/evaluate_rule`, { rule_name: ruleName, data }, config)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error evaluating rules:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.post(`${API_URL}/evaluate_rule`, { rule_name: ruleName, data }, config).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 };
 
 export const modifyRule = async (ruleName: string, rule: string) => {
-    return axios.post(`${API_URL}/modify_rule`, { rule, name: ruleName }, config)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error modifying rules:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.post(`${API_URL}/modify_rule`, { rule, name: ruleName }, config).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 };
 
 export const deleteRule = async (ruleName: string) => {
-    return axios.delete(`${API_URL}/delete_rule?rule_name=${ruleName}`)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error deleting rules:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.delete(`${API_URL}/delete_rule?rule_name=${ruleName}`).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 }
 export const getRule = async (ruleName: string) => {
-    return axios.get(`${API_URL}/get_rule?rule_name=${ruleName}`)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error fetching rule:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.get(`${API_URL}/get_rule?rule_name=${ruleName}`).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 }
 
 export const getAllRuleNames = async () => {
-    return axios.get(`${API_URL}/get_all_rule_names`)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error fetching rules:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.get(`${API_URL}/get_all_rule_names`).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 }
 
 export const getCatalog = async () => {
-    return axios.get(`${API_URL}/get_catalog`)
-        .then((response) => response.data)
-        .catch((error) => {
-            if (axios.isAxiosError(error)) {
-                console.error(error.response?.data.detail);
-                throw error.response?.data?.detail;
-            } else {
-                console.error('Unknown error fetching catalog:', error);
-                throw error;
-            }
-        });
+    try{
+        return axios.get(`${API_URL}/get_catalog`).then((response) => response.data)
+    } catch (error) {
+        handleApiError(error);
+    }
 }
